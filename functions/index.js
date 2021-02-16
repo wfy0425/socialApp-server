7a85b0f7bfd3af98007177a9ff17006faafc075a
Fengyuan Wu <wfy0425@yeah.net>
2021-02-16 10:12:21 +0800

    fix cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require('firebase-functions')
 const app = require('express')()
 const FBAuth = require('./util/fbAuth')
+
+const cors = require('cors')
+app.use(cors())
+
 const { db } = require('./util/admin')
 const {
   getAllScreams,
